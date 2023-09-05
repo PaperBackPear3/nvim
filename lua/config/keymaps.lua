d15661f0ad65273ff7e1fb30a637dd61ee50b7ce
PaperBackPear3 <fabioangeloni2001@gmail.com>
2023-09-06 00:39:28 +0200

    first setup, added telescope,lazy.nvim,treesitter

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,38 @@
+---------------------
+-- Control family
+---------------------
+-- Ctrl F to format the file.
+vim.keymap.set("n", "<C-F>", ":Format<cr>", { noremap = true, silent = true })
+-- Ctrl E to clean up trailing whitespaces
+vim.keymap.set("n", "<C-E>", ":%s/\\s*$//g<cr>", { noremap = true })
+-- get out of term
+vim.keymap.set("t", "<Esc>", "<C-\\><C-n>", { noremap = true })
+
+---------------------
+-- leader family
+---------------------
+-- buffer navigation
+vim.keymap.set("n", "<leader>a", ":bprevious<cr>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>s", ":bnext<cr>", { noremap = true, silent = true })
+-- show undotree
+vim.keymap.set("n", "<leader>u", ":UndotreeToggle<cr>", { noremap = true, silent = true })
+
+-- split windows
+vim.keymap.set("n", "<leader>h", ":FocusSplitLeft<cr>", { silent = true })
+vim.keymap.set("n", "<leader>j", ":FocusSplitDown<cr>", { silent = true })
+vim.keymap.set("n", "<leader>k", ":FocusSplitUp<cr>", { silent = true })
+vim.keymap.set("n", "<leader>l", ":FocusSplitRight<cr>", { silent = true })
+
+-- toggle terminal
+vim.keymap.set("n", "<leader>t", '<CMD>lua require("FTerm").toggle()<CR>', { noremap = true, silent = true })
+vim.keymap.set("t", "<leader>t", '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>', { noremap = true, silent = true })
+
+-- toggle lsp errors
+vim.keymap.set("n", "<leader>e", "<cmd>TroubleToggle<cr>", { noremap = true, silent = true })
+
+-- Telescope searches
+vim.keymap.set("n", "<leader>f", ":Telescope find_files<cr>", { noremap = true, silent = false })
+vim.keymap.set("n", "<leader>g", ":Telescope live_grep<cr>", { noremap = true, silent = false })
+
+-- LSP searches
+vim.keymap.set("n", "<leader>d", "<cmd>lua vim.lsp.buf.definition()<cr>", { noremap = true, silent = false })
---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,17 @@
+return {
+	{
+    'nvim-telescope/telescope.nvim', tag = '0.1.2', enabled=true,dependencies = { 'nvim-lua/plenary.nvim' },
+	  defaults = {
+    -- Default configuration for telescope goes here:
+    -- config_key = value,
+    mappings = {
+      i = {
+        -- map actions.which_key to <C-h> (default: <C-/>)
+        -- actions.which_key shows the mappings for your picker,
+        -- e.g. git_{create, delete, ...}_branch for the git_branches picker
+        ["<C-h>"] = "which_key"
+      }
+    }
+  },
+}
+}
